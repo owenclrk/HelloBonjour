30bafe6ba3b436c1a39dcc4317305784323963eb
owenclrk <owenclrk@gmail.com>
2026-05-25 18:57:19 +0200

    Adding initial code to generate buttons

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,32 @@ import api from './api'
 import './App.css';
 
 const App = () => {
-  const [results, setResults] = useState([]);
-  const [formData, setFormData] = useState({
-    text: '',
-    result: '',
-    lang: 'FR'
-  });
+const [results, setResults] = useState([]);
+const [questions, setQuestions] = useState({
+  prompt: '',
+  answer: ''
+});
+// const [questions, setQuestions] = useState([])
+const [formData, setFormData] = useState({
+  text: '',
+  result: '',
+  lang: 'FR'
+});
 
-  const fetchResults = async () => {
-    const response = await api.get('/dict/');
-    setResults(response.data)
-  };
+const fetchResults = async () => {
+  const response = await api.get('/dict/');
+  setResults(response.data)
+};
+
+const fetchQuestions = async () => {
+  const questions = await api.get('/questions/')
+  setQuestions(questions.data)
+}
+
+  //   const fetchQuestions = async () => {
+  //   const questions = await api.post('/dict/');
+  //   setQuestions(questions.data)
+  // };
 
   useEffect(() => {
     fetchResults();
@@ -50,11 +65,21 @@ const App = () => {
       }
   };
 
+  const handleQuestions = async (event) => {
+    event.preventDefault();
+    await api.post('/questions/', formData);
+    console.log('formData',formData);
+    fetchQuestions();
+  }
+  
+
+
 return (
+  //Container
 <div className="app-container">
+
   <div className="card">
     <h1 className="title">Translation Dictionary</h1>
-
   <form className="item-form" onSubmit={handleFormSubmit}>
       <div className="input-group">
         <label>Word to Be Translated</label>
@@ -69,7 +94,6 @@ return (
           placeholder='Word to be translated'
         />
       </div>
-
       <div className="input-group">
         <label>Language</label>
         <select
@@ -83,50 +107,63 @@ return (
         # Need to add default or else react won't update the table
         <option value="" disabled>Select a language</option> {/* Placeholder */}
         <option value="FR"> French</option>
+        <option value="EN-GB"> English </option>
         </select>
       </div>
-
-
       <button type="submit" className="submit-btn">Submit</button>
+
     </form>
 
-        {/* 3. Centered Table Section */}
-          <h2 className="subtitle">Translation List</h2>
-              <div className="table-container">
-                <table className="items-table">
-                <thead className='table-primary'>
-                  <tr>
-                    <th>Word</th>
-                    <th>Language</th>
-                    <th>Translation</th>
-                    <th>Remove</th>
-                  </tr>
-                </thead>
-                <tbody className="align-middle">
-                  {results.map((result) => (
-                    <tr key={result.id}>
-                      <td className="fw-medium">{result.text}</td>
-                      <td className="text-muted">{result.lang}</td>
-                      <td className="text-primary fw-bold">{result.result}</td>
-                      <td>
-                        <button onClick={() => handleDelete(result.id)} className="btn btn-outline-danger btn-sm px-3">
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                  {results.length === 0 && (
-                    <tr>
-                      <td colSpan="3" className="text-center py-4 text-muted">
-                        No translation found. Add one above!
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </div>
+
+      <h2 className="subtitle">History</h2>
+          <div className="table-container">
+            <table className="items-table">
+            <thead className='table-primary'>
+              <tr>
+                <th>Word</th>
+                <th>Language</th>
+                <th>Translation</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody className="align-middle">
+              {results.map((result) => (
+                <tr key={result.id}>
+                  <td className="fw-medium">{result.text}</td>
+                  <td className="text-muted">{result.lang}</td>
+                  <td className="text-primary fw-bold">{result.result}</td>
+                  <td>
+                    <button onClick={() => handleDelete(result.id)} className="btn btn-outline-danger btn-sm px-3">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+              {results.length === 0 && (
+                <tr>
+                  <td colSpan="3" className="text-center py-4 text-muted">
+                    No translation found. Add one above!
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
+      
+      {/* End of history page */}
+      {/* Generate list of questions */}
+      <button onClick={() => handleQuestions()} className="submit-btn">
+        Generate Questions
+      </button>
+            <tbody className="align-middle">
+              {/* {questions.map((question) => (
+                <tr key={question.id}>
+                  <td className="fw-medium">{question.prompt}</td>
+                </tr>
+              ))} */}
+            </tbody>
+      </div>
+    </div>
   );
 }
 
